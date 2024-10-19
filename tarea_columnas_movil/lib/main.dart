5a29d18ebc887476cf8caece54f4d00fce4cec09
Lalo Lara <lalolaraprado@gmail.com>
2024-10-18 20:40:46 -0600

    Lalo

--- tarea_columnas_movil/lib/main.dart
+++ tarea_columnas_movil/lib/main.dart
@@ -1,85 +1,81 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// Función principal que inicia la ejecución de la aplicación.
 
-class MyApp extends StatelessWidget {
+
+class Productos extends StatelessWidget{
+  final String usuario;
+
+  Productos ({required this.usuario});
+
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Tienda Virual',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomeScreen(),
-    );
+  Widget build (BuildContext context) {
+    return TiendaScreen();
   }
 }
 
-class HomeScreen extends StatelessWidget {
+// Clase principal de la aplicación que extiende StatelessWidget,
+class TiendaScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Tienda Valorant Virtual'),
-      ),
-      body: SingleChildScrollView( // Permite desplazarse si hay mucho contenido
-        child: Column(
+    return MaterialApp(
+      // Utiliza un Scaffold para definir la estructura básica de la pantalla,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tienda en Línea'), // Título que aparece en la barra de la aplicación.
+        ),
+        body: ListView( // ListView permite hacer scroll en una lista de widgets.
           children: [
-            // Primer recuadro
-            InfoBox(
-              text: 'Zapatos',
-              imageUrl: 'https://supershoes.mx/2640-large_default/zapatos-casuales-de-piel-flexi-63202-843-negro-caballero.jpg', // URL de imagen
+            // Llama a la función productCard para crear una tarjeta de producto.
+            productCard(
+              'https://yazbek.com.mx/cdn/shop/products/C0605-camisa-oxford-liso-ml-caballero-75algodon-25poliester-cobalto_1.jpg?v=1693281109',
+              'Camisa',
+              '40.00 Pesos',
+              Colors.indigo[50],
             ),
-            // Segundo recuadro
-            InfoBox(
-              text: 'Playera',
-              imageUrl: 'https://nao.com.mx/cdn/shop/products/14-Verde-800x800.jpg?v=1605882871', // URL de imagen
+            Divider(), // Crea una línea divisoria entre productos.
+            productCard(
+              'https://nao.com.mx/cdn/shop/products/14-Verde-800x800.jpg?v=1605882871',
+              'Plauera',
+              '60.00 Pesos',
+              Colors.pink[50],
             ),
-            // Tercer recuadro
-            InfoBox(
-              text: 'Sudadera',
-              imageUrl: 'https://fos.com.mx/cdn/shop/files/carbonjaspeado_front_sudadera3320.png?v=1699923596', // URL de imagen
+            Divider(), // Línea divisoria entre productos.
+            productCard(
+              'https://fos.com.mx/cdn/shop/files/carbonjaspeado_front_sudadera3320.png?v=1699923596',
+              'Sudadera',
+              '80.00 Pesos',
+              Colors.purple[50],
             ),
-            // Puedes agregar más recuadros si es necesario
           ],
         ),
       ),
     );
   }
-}
 
-class InfoBox extends StatelessWidget {
-  final String text;
-  final String imageUrl;
-
-  InfoBox({required this.text, required this.imageUrl});
-
-  @override
-  Widget build(BuildContext context) {
+  // Función que crea una tarjeta de producto personalizada.
+  Widget productCard(String imageUrl, String description, String price, Color? color) {
     return Container(
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Colors.blue[100],
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 3,
-            blurRadius: 5,
-            offset: Offset(0, 3),
-          ),
-        ],
-      ),
+      color: color,
+      padding: EdgeInsets.all(16.0), // Agrega espacio interno alrededor del contenido.
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center, // Alinea los elementos hijos al centro.
         children: [
-          Image.network(imageUrl), // Cargar imagen desde la URL
-          SizedBox(height: 10),
+          Image.network(
+            imageUrl,
+            height: 150,
+            width: 150,
+          ),
+          SizedBox(height: 8),
           Text(
-            text,
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            description,
+            style: TextStyle(fontSize: 16), // Tamaño de fuente para la descripción.
+          ),
+          SizedBox(height: 4), // Espacio entre la descripción y el precio.
+          Text(
+            price,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold), // Precio en negrita.
           ),
         ],
       ),
